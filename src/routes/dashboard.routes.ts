df197f19cb2ad12741261dd10867f19c4e5b3451
Swar Bhatia <swarbhatia@gmail.com>
2026-06-15 02:49:32 +0530

    feat: add instructor dashboard with revenue and purchase metrics

---
+++ src/routes/dashboard.routes.ts
@@ -0,0 +1,69 @@
+import { Request, Router } from "express";
+const router=Router();
+import prisma from "../lib/prisma";
+import {VerifyUser, InstructorOnly } from "../middleware/auth";
+type AuthenticatedRequest = Request & {
+    user?: {
+        userId: number;
+        role: string;
+    };
+};
+router.get("/student",(req,res)=>{
+
+})
+router.get("/instructor",VerifyUser,InstructorOnly, async (req:AuthenticatedRequest,res)=>{
+    // first calculate the total courses of the instructor 
+    const instructorId=req.user!.userId;
+
+    const [
+        totalCourses,
+        coursePurchases,
+        courses
+    ] = await Promise.all([
+        prisma.course.count({
+            where: {
+                authorId: instructorId
+            }
+        }),
+
+        prisma.purchase.count({
+            where: {
+                course: {
+                    authorId: instructorId
+                }
+            }
+        }),
+
+        prisma.course.findMany({
+            where: {
+                authorId: instructorId
+            },
+            select: {
+                price: true,
+                _count: {
+                    select: {
+                        purchases: true
+                    }
+                }
+            }
+        })
+    ]);
+
+    const totalRevenue = courses.reduce(
+        (sum, course) =>
+            sum + course.price * course._count.purchases,
+        0
+    );
+
+
+    return res.json({
+        totalCourses,
+        coursePurchases,
+        totalRevenue
+    })
+
+})
+
+
+
+export default router;
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import authRouter from "./routes/auth.routes";
 import courseRouter from "./routes/course.routes";
+import dashboardRouter from "./routes/dashboard.routes"
 const app = express();
 
 app.use(express.json());
@@ -16,6 +17,7 @@ app.get("/", (req, res) => {
 });
 app.use("/auth", authRouter);
 app.use("/courses",courseRouter);
+app.use("/dashboard",dashboardRouter);
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
